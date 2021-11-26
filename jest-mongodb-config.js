bb79b38b92f8b343b0424c90856b069c62e7c677
Fernando de Abreu Pereira <fernandodeabreupereira@gmail.com>
2021-11-26 16:21:13 -0300

    refactor: adjust configs jest for mongodb

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  mongoDbMemoryServerOptions: {
-    instance: {
-      dbName: 'jest'
-    },
+  mongodbMemoryServerOptions: {
     binary: {
       version: '4.0.3',
       skipMD5: true
+    },
+    instance: {
+      dbName: 'jest'
     },
     autoStart: false
   }
